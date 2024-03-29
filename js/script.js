8b48861c615097e5a3a904af492546de707fb795
adam woodson <awoodson.contact@gmail.com>
2024-03-29 18:13:42 -0500

    fixed for gitpages

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var requestId = requestAnimationFrame(moveCursor);
 
     // Add active class to HOME link when on index.html or /
-    if (window.location.pathname === 'index.html' || window.location.pathname === '/') {
+    if (window.location.pathname === '/index.html' || window.location.pathname === '/') {
         document.querySelector('.home-link').classList.add('active');
     }
 
@@ -52,20 +52,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Check if on aboutme.html page and add active class to About Me link
-    if (window.location.pathname === 'aboutme.html') {
+    if (window.location.pathname === '/aboutme.html') {
         document.querySelector('.nav-box a[href="aboutme.html"]').parentElement.classList.add('active');
-        document.querySelector('.container').style.backgroundImage = "url('images/AboutMe.png')";
+        document.querySelector('.container').style.backgroundImage = "url('/images/AboutMe.png')";
     }
 
     // Check if on portfolio.html page and add active class to Portfolio link
-    if (window.location.pathname === 'portfolio.html') {
+    if (window.location.pathname === '/portfolio.html') {
         document.querySelector('.portfolio-link').classList.add('active')
-        document.querySelector('.container').style.backgroundImage = "url('images/folio.png')";
+        document.querySelector('.container').style.backgroundImage = "url('/images/folio.png')";
     }
 
-    if (window.location.pathname === 'contact.html') {
+    if (window.location.pathname === '/contact.html') {
         document.querySelector('.contact-link').classList.add('active')
-        document.querySelector('.container').style.backgroundImage = "url('images/contact.png')";
+        document.querySelector('.container').style.backgroundImage = "url('/images/contact.png')";
     }
 
     // Add event listeners to handle image swap on hover for email images
@@ -84,5 +84,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     soonImg.addEventListener('mouseout', function () {
         soonImg.src = 'images/soonBefore.png';
+    });
+
+    var discordImg = document.getElementById('discord-img');
+    discordImg.addEventListener('mouseover', function () {
+        discordImg.src = 'images/discordAfter.png';
+    });
+    discordImg.addEventListener('mouseout', function () {
+        discordImg.src = 'images/discordBefore.png';
     });
 });
