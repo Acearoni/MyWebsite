27b435623a28cb3fcefaf4e4c14f88df3a8e8739
adam woodson <awoodson.contact@gmail.com>
2024-03-29 20:34:50 -0500

    potential img workaround

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var requestId = requestAnimationFrame(moveCursor);
 
     // Add active class to HOME link when on index.html or /
-    if (window.location.pathname === '/index.html' || window.location.pathname === '/') {
+    if (window.location.pathname == '/index.html' || window.location.pathname == '/') {
         document.querySelector('.home-link').classList.add('active');
         console.log("Active Window INDEX");
     }
@@ -40,9 +40,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get the current URL path and remove the leading slash
     var path = window.location.pathname.substr(1);
     // Adjust path for GitHub Pages deployment
-    if (window.location.hostname === 'acearoni.github.io') {
+    if (window.location.hostname == 'acearoni.github.io') {
         path = '/MyWebsite/' + path;
     }
+
+    // Log the current path to debug
+    console.log("Current Path: ", path);
 
     // Loop through all nav-box elements
     document.querySelectorAll(".nav-box a").forEach(function (element) {
@@ -50,65 +53,53 @@ document.addEventListener("DOMContentLoaded", function () {
         var href = element.getAttribute("href").substr(1);
 
         // Check if the href matches the current URL path
-        if (href === path) {
+        if (href == path) {
             // Add the active class to the parent nav-box element
             element.parentElement.classList.add("active");
         }
     });
 
-    // Check if on aboutme.html page and add active class to About Me link
-    if (path === 'aboutme.html') {
-        document.querySelector('.nav-box a[href="aboutme.html"]').parentElement.classList.add('active');
-        document.querySelector('.container').style.backgroundImage = "url('https://acearoni.github.io/MyWebsite/images/AboutMe.png')";
-        console.log(path);
-    }
+    // // Check if on aboutme.html page and add active class to About Me link
+    // if (path == 'aboutme.html') {
+    //     document.querySelector('.nav-box a[href="aboutme.html"]').parentElement.classList.add('active');
+    //     document.querySelector('.container').style.backgroundImage = "url('./images/AboutMe.png')";
+    //     console.log(path);
+    // }
 
     // Check if on portfolio.html page and add active class to Portfolio link
-    if (path === 'portfolio.html') {
+    if (path == 'portfolio.html') {
         document.querySelector('.portfolio-link').classList.add('active')
-        document.querySelector('.container').style.backgroundImage = "url('https://acearoni.github.io/MyWebsite/images/folio.png')";
+        document.querySelector('.container').style.backgroundImage = "url('./images/folio.png')";
     }
 
-    if (path === 'contact.html') {
+    if (path == 'contact.html') {
         document.querySelector('.contact-link').classList.add('active')
-        document.querySelector('.container').style.backgroundImage = "url('https://acearoni.github.io/MyWebsite/images/contact.png')";
+        document.querySelector('.container').style.backgroundImage = "url('./images/contact.png')";
     }
 
     // Add event listeners to handle image swap on hover for email images
     var emailImg = document.getElementById('email-img');
-    if (emailImg) {
-        emailImg.addEventListener('mouseover', function () {
-            emailImg.src = './images/emailAfter.png';
-        });
-        emailImg.addEventListener('mouseout', function () {
-            emailImg.src = './images/emailBefore.png';
-        });
-    } else {
-        console.error('email-img element not found!');
-    }
+    emailImg.addEventListener('mouseover', function () {
+        emailImg.src = './images/emailAfter.png';
+    });
+    emailImg.addEventListener('mouseout', function () {
+        emailImg.src = './images/emailBefore.png';
+    });
 
     // Add event listeners to handle image swap on hover for soon images
     var soonImg = document.getElementById('soon-img');
-    if (soonImg) {
-        soonImg.addEventListener('mouseover', function () {
-            soonImg.src = './images/soonAfter.png';
-        });
-        soonImg.addEventListener('mouseout', function () {
-            soonImg.src = './images/soonBefore.png';
-        });
-    } else {
-        console.error('soon-img element not found!');
-    }
+    soonImg.addEventListener('mouseover', function () {
+        soonImg.src = './images/soonAfter.png';
+    });
+    soonImg.addEventListener('mouseout', function () {
+        soonImg.src = './images/soonBefore.png';
+    });
 
     var discordImg = document.getElementById('discord-img');
-    if (discordImg) {
-        discordImg.addEventListener('mouseover', function () {
-            discordImg.src = './images/discordAfter.png';
-        });
-        discordImg.addEventListener('mouseout', function () {
-            discordImg.src = './images/discordBefore.png';
-        });
-    } else {
-        console.error('discord-img element not found!');
-    }
+    discordImg.addEventListener('mouseover', function () {
+        discordImg.src = './images/discordAfter.png';
+    });
+    discordImg.addEventListener('mouseout', function () {
+        discordImg.src = './images/discordBefore.png';
+    });
 });
